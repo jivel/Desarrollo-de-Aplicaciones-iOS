d2ea99a8d2a1a90168e50de832d603b0e532a0ef
Luis Antonio Jiménez Velasco <luis.antonio.jimenez.velasco@gmail.com>
2016-04-25 00:22:39 -0500

    Cambiar color
    Cambiar color a fondo de pantalla

--- Swift programar para iOS/Hamburguesas en el mundo/Hamburguesas en el mundo/ViewController.swift
+++ Swift programar para iOS/Hamburguesas en el mundo/Hamburguesas en el mundo/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cambiarColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
