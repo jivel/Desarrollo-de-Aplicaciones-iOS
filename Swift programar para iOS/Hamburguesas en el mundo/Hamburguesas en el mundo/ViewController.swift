4a33bfbfe02da13d3bb0930e7372e4b594060b3a
Luis Antonio Jiménez Velasco <luis.antonio.jimenez.velasco@gmail.com>
2016-04-24 23:49:22 -0500

    Tarea: Hamburguesas en el mundo!
    Crea una aplicación que le muestra al usuario las hamburguesas del
    mundo, con un precio aleatorio.

---
+++ Swift programar para iOS/Hamburguesas en el mundo/Hamburguesas en el mundo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Hamburguesas en el mundo
+//
+//  Created by Luis Antonio Jimenez Velasco on 24/04/16.
+//  Copyright © 2016 Luis Antonio Jimenez Velasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nombrePais: UILabel!
+
+    @IBOutlet weak var nombreHamburguesa: UILabel!
+    
+    let paises = ColeccionDePaises()
+    
+    let hamburguesas = ColeccionDeHamburguesa()
+    
+    let colores = Colores()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func quieroUnaHamburguesa(sender: AnyObject) {
+        cambiarColor()
+        self.nombrePais.text = self.paises.obtenPais()
+        self.nombreHamburguesa.text = self.hamburguesas.obtenHamburguesa()
+    }
+    
+    func cambiarColor() {
+        let color = self.colores.obtenerColor()
+        view.backgroundColor = color
+        view.tintColor = color
+    }
+
+}
+
